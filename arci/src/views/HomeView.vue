c4d4b6f914cf2e9c974b27953b34d216570d5c94
SantiagoGRON <sa.garciariv9@utsoe.edu.mx>
2023-10-20 13:31:18 -0600

    Subida de página

---
+++ arci/src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+    <main>
+        <header>
+            <meta charset="utf-8" />
+            <title>ARCI</title>
+            <h1 align="center">ARCI</h1>
+        </header>
+        <body>
+            <h2>Equipo 3</h2>
+            <ul>
+                <li>Santiago Garcia Rivera</li>
+                <li>Marco Antonio Contreras Gonzalez</li>
+            </ul>
+            <img src="https://gifdb.com/images/high/club-penguin-dance-200-x-200-gif-znfusk83270n036o.gif">
+        </body>
+    </main>
+</template>
